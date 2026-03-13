a925bcf1da5f646fc58169c09e962ffbd20c6bad
Jonny <jonnylloydie1@gmail.com>
2026-03-13 12:18:07 +0000

    re-hash

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+
+# Variables
+KEY_PATH=~/.ssh/jonny_cloud_deployment.pem
+EC2_HOST=ec2-user@3.10.180.190
+REMOTE_DIR=/home/ec2-user/message-server
+
+# Copy necessary files
+echo "> Uploading files to EC2"
+scp -i $KEY_PATH app.py requirements.txt Dockerfile $EC2_HOST:$REMOTE_DIR
+
+# Connect and run container
+ssh -i $KEY_PATH $EC2_HOST << EOF
+  cd message-server
+  docker build -t message-server .
+  docker stop message-server || true
+  docker rm message-server || true
+  docker run -p 5002:5002 \
+    -e APP_ENV=PRODUCTION \
+    -e POSTGRES_USER=postgres \
+    -e POSTGRES_PASSWORD=hello123__12 \
+    -e POSTGRES_HOST=jonny-message-server-db.cvruukypsgyb.eu-west-2.rds.amazonaws.com \
+    -e POSTGRES_DB=postgres \
+    message-server
+EOF
+
+echo "✅ Deployment complete! Visit: http://3.10.180.190:5002"
